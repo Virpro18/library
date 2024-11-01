ea8acc1af01fbb19e1df7ababbb42e288d724557
Virpro18 <sintawidodo032@gmail.com>
2024-11-02 00:24:32 +0900

    try again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import Cards from "@/components/Cards";
-import { LibraryData } from "@/types/database";
-import {  jsonDatabase } from "@/components/lib/database";
+// import Cards from "@/components/Cards";
+// import { LibraryData } from "@/types/database";
+// import {  jsonDatabase } from "@/components/lib/database";
 export default async function Home() {
-  const datas:LibraryData = await jsonDatabase();
+  // const datas:LibraryData = await jsonDatabase();
   // if (error) {
   //   return <p>{error.message}</p>;
   // }
   return (
     <div>
       <section className="grid md:grid-cols-4 grid-cols-auto w-full gap-4 mt-4 p-4">
-      {datas.data.map((data, index) => [<Cards key={index} data={data} />])}
+      {/* {datas.data.map((data, index) => [<Cards key={index} data={data} />])} */}
       </section>
     </div>
   );
