6fd2df2b336ccf8cb160d1fb8817af712b242280
Virpro18 <sintawidodo032@gmail.com>
2024-11-01 23:53:27 +0900

    remove supabase

--- src/components/lib/database.ts
+++ src/components/lib/database.ts
@@ -1,21 +1,21 @@
-import { createClient } from "@supabase/supabase-js";
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const key = process.env.NEXT_PUBLIC_SUPABASE_PASS;
+// import { createClient } from "@supabase/supabase-js";
+// const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+// const key = process.env.NEXT_PUBLIC_SUPABASE_PASS;
 
 type database = {
   data: string;
 };
 
-if (!url || !key) {
-  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL");
-}
-export const supaBase = createClient(url, key);
+// if (!url || !key) {
+//   throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL");
+// }
+// export const supaBase = createClient(url, key);
 
-export const database = async (db: string, col: string = "") => {
-  const data = await supaBase.from(`${db}`).select(`${col}`);
-  console.log(data);
-  return data;
-};
+// export const database = async (db: string, col: string = "") => {
+//   const data = await supaBase.from(`${db}`).select(`${col}`);
+//   console.log(data);
+//   return data;
+// };
 
 export const jsonDatabase = async (data?: database) => {
   const rawJson = await fetch("http://localhost:3000/api/database/recieveData", {
